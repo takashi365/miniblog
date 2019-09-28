a0db219c74c8b66f7339aefe12cc068db165f8cd
takashi365 <yuda365@gmail.com>
2019-09-28 19:23:15 +0900

    基本実装完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,39 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, except: :index
   def index
+    @articles = Article.includes(:user).order("created_at DESC").page(params[:page]).per(3)
   end
+
+  def new
+  end
+
+  def create
+    Article.create(title: article_params[:title], article: article_params[:article], user_id: current_user.id)
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy if article.user_id == current_user.id
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      article.update(article_params)
+    end
+  end
+
+  private
+  def article_params
+    params.permit(:title, :article)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "articles#index"
-  get 'articles' => 'articles#index'
-
+  resources :articles, except: [:show]
 end
